7c4fb3823bfb9e643112ee225082a1e73cd50ef6
Aslan <pro@insightit.kz>
2026-09-16 16:44:52 +0500

    Add Yandex.Metrika and Google Analytics (GA4) counters

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -94,6 +94,32 @@ export default async function LocaleLayout({
         <Script id="bitrix24-crm-widget" strategy="afterInteractive">
           {`(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.kz/b27351276/crm/site_button/loader_1_56dniu.js');`}
         </Script>
+        <Script
+          id="gtag-js"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-BX6Z6416PR"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BX6Z6416PR');`}
+        </Script>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112712468', 'ym');
+          ym(112712468, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="https://mc.yandex.ru/watch/112712468" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   );
